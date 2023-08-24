d083e4e8c109e312a0688965967d8ea9c01042a2
Ali <plewffy@gmail.com>
2023-08-24 09:37:34 +0600

    again

--- src/components/UI/Logo.jsx
+++ src/components/UI/Logo.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const Logo = () => {
     return (
-        <Link to='/eatly' className="logo">
+        <Link to='/eatly/' className="logo">
             <img data-aos="fade-right" data-aos-delay="100" src="/logo.svg" alt="eatly" />
             <span data-aos="fade-right" data-aos-delay="200">eatly</span>
         </Link>
